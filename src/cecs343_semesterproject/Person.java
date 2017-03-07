9bd172e9f706742e48c9cbe42be2a14993adfa19
elenatsoi <elenatsoi@gmail.com>
2017-03-07 11:31:48 -0800

    Person class created. Only attributes added.

---
+++ src/cecs343_semesterproject/Person.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cecs343_semesterproject;
+
+/**
+ *
+ * @author Elena
+ */
+public class Person {
+    
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address1;
+    private String city;
+    private String state;
+    private String zip;
+}
